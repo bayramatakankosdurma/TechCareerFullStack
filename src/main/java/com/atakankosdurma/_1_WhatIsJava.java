632c1db7d93faecefac85f17d98356e874bbd24a
Bayram Atakan Koşdurma <atakan.kosdurma@gmail.com>
2024-04-29 12:06:39 +0300

    Second commit of the day

--- src/main/java/com/atakankosdurma/_1_WhatIsJava.java
+++ src/main/java/com/atakankosdurma/_1_WhatIsJava.java
@@ -1,2 +1,12 @@
-package com.atakankosdurma;public class _1_WhatIsJava {
+package com.atakankosdurma;
+
+public class _1_WhatIsJava {
+
+    // yorum slm
+    /* yorum slm
+    * multi
+    * line */
+    public static void main(String[] args){
+        System.out.println("Merhabalar Nasılsınız");
+    }
 }
