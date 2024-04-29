3e97c50dc146f0798b5156acf33b8a5941a52682
Bayram Atakan Koşdurma <atakan.kosdurma@gmail.com>
2024-04-29 12:16:19 +0300

    get to know primitive types

--- src/main/java/com/atakankosdurma/_1_WhatIsJava.java
+++ src/main/java/com/atakankosdurma/_1_WhatIsJava.java
@@ -8,5 +8,27 @@ public class _1_WhatIsJava {
     * line */
     public static void main(String[] args){
         System.out.println("Merhabalar Nasılsınız");
+        // variables
+
+        //not float
+        byte b1=25;
+        short s1 = 125;
+        int i1 = 1255;
+        long l1 = 4324234238743287232L;
+        System.out.println("b1 = "+ b1);
+
+        //float numbers
+        float f2 = 14.53f;
+        double d2 = 12321312.34243242;
+
+        //characters
+        char c3 = '2';
+        boolean b3 = true;
+
+        //primitive type - Wrapper type
+        int primitive = 44;
+        //primitive types cannot be null
+        Integer objectInt = 44;
+        objectInt = null;
     }
 }
